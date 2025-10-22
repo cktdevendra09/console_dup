c50e21adfd543feb44f7012ac51638253ac5f63b
Devendra Kumar <cktdevendra955@gmail.com>
2025-10-22 20:12:21 +0530

    1.2.3

--- app/(web)/layout.js
+++ app/(web)/layout.js
@@ -1,9 +1,28 @@
+'use client';
+import { usePathname } from "next/navigation";
 import FooterUI from "./components/layout/FooterUI";
 import NavbarUI from "./components/layout/NavbarUI";
+import { useEffect, useState } from "react";
 
 export default function WebsiteLayout({ children }) {
+
+  const pathname = usePathname(); usePathname
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    setLoading(true);
+    const timeout = setTimeout(() => setLoading(false), 500); // simulate smooth transition
+    return () => clearTimeout(timeout);
+  }, [pathname]);
+
   return (
     <div className="bg-gray-50">
+      {loading && (
+        <div className="fixed inset-0 flex items-center justify-center bg-white/80 backdrop-blur-sm z-50 transition-opacity duration-300">
+          <div className="w-12 h-12 border-4 border-blue-500 border-t-transparent rounded-full animate-spin"></div>
+        </div>
+      )}
+      
       <NavbarUI></NavbarUI>
       <main className="min-h-screen">{children}</main>
       <FooterUI />
--- app/dashboard/layout.js
+++ app/dashboard/layout.js
@@ -1,22 +1,39 @@
 'use client';
 import "./styles/globals.css";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Sidebar from "./components/layout/sidebar/Sidebar";
 import Header from "./components/layout/header/Header";
+import { usePathname } from "next/navigation";
 
 export default function RootLayout({ children }) {
   const [open, setOpen] = useState(false);
+
+    const pathname = usePathname(); usePathname
+    const [loading, setLoading] = useState(false);  
+    useEffect(() => {
+      setLoading(true);
+      const timeout = setTimeout(() => setLoading(false), 500); // simulate smooth transition
+      return () => clearTimeout(timeout);
+    }, [pathname]);
+  
   return (
     
       <>
 
-        <div className="min-h-screen bg-gray-50 text-gray-900">      
+        <div className="min-h-screen bg-gray-50 text-gray-900">   
+             
               <div className="mx-auto grid max-w-[1400px] grid-cols-1 gap-4 md:grid-cols-[18rem_1fr]">
                 <Sidebar open={open} onClose={() => setOpen(false)} />
                 <main className="space-y-4">
                   <div className="">
                     <Header onMenu={() => setOpen(true)} />
-                  </div>          
+                  </div>  
+
+                  {loading && (
+        <div className="fixed inset-0 flex items-center justify-center bg-white/80 backdrop-blur-sm z-50 transition-opacity duration-300">
+          <div className="w-12 h-12 border-4 border-blue-500 border-t-transparent rounded-full animate-spin"></div>
+        </div>
+      )}        
 
                   {children}
                 </main>
